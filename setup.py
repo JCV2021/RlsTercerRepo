d02090f54fac5df4ab7a64f31711d6bc74fc6515
jvc2021 <jcvizcarra2021@gmail.com>
2026-02-14 19:42:02 -0500

    mi primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RlsTercerRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="JuanCarlos",                            # Tu nombre
+    author_email="jcvizcarra2021@gmail.com",         # Tu correo electrónico
+    url="https://github.com/JVC2021/RlsTercerRepo",     # URL del proyecto
+)
